becbc67eeb3d9cf0214230a13c15f09e367a2152
Zabrimus <github@hannebauer.org>
2023-06-26 17:49:04 +0200

    more flexible start.sh

--- cefbrowser-base-docker/start.sh
+++ cefbrowser-base-docker/start.sh
@@ -3,13 +3,21 @@
 ASUSER=false
 INIFILE=sockets.ini
 
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        -user) shift; ASUSER=true ;;
-        -ini) shift; INIFILE=$1;;
-    esac
-    shift || continue
+while [ "$#" -gt 0 ]; do
+  case $1 in
+    -user) shift ; ASUSER=true;;
+    -ini) shift ; INIFILE=$1; shift;;
+    --) shift; break;;
+    *) exit 1 # should never be reached.
+  esac
 done
+
+if [ "$#" -eq 0 ]; then
+    # set default parameter
+    ARGS="--enable-features=UseOzonePlatform --ozone-platform=headless"
+else
+    ARGS="$*"
+fi
 
 USER_ID=${UID:=$USER_ID}
 USER_GID=${GID:=$USER_GID}
@@ -19,7 +27,7 @@ groupmod -g $USER_GID ${USER}
 usermod  -g $USER_ID  ${USER}
 
 if [ "${ASUSER}" = "true" ]; then
-  sudo -E -u ${USER}  LD_LIBRARY_PATH=/app ./cefbrowser --config=${INIFILE} --off-screen-rendering-enabled --enable-features=UseOzonePlatform --ozone-platform=headless
+  sudo -E -u ${USER}  LD_LIBRARY_PATH=/app ./cefbrowser --config=${INIFILE} --off-screen-rendering-enabled $ARGS
 else
-  LD_LIBRARY_PATH=/app ./cefbrowser --config=${INIFILE} --off-screen-rendering-enabled --enable-features=UseOzonePlatform --ozone-platform=headless
+  LD_LIBRARY_PATH=/app ./cefbrowser --config=${INIFILE} --off-screen-rendering-enabled $ARGS
 fi
